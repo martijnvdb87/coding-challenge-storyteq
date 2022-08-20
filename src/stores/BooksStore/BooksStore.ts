237886bd5c2031409921811002191e446505b275
Martijn van den Bosch <martijn_van_den_bosch@hotmail.com>
2022-08-20 18:48:43 +0200

    Added BooksStore

---
+++ src/stores/BooksStore/BooksStore.ts
@@ -0,0 +1,39 @@
+import { ref } from "vue";
+
+export type Book = {
+  title: string;
+  author: string;
+};
+
+const store = ref<Book[]>([
+  {
+    title: "Don Quixote",
+    author: "Miguel De Cervantes",
+  },
+  {
+    title: "Pilgrim's Progress",
+    author: "John Bunyan",
+  },
+  {
+    title: "Robinson Crusoe",
+    author: "Daniel Defoe",
+  },
+  {
+    title: "Gulliver's Travels",
+    author: "Jonathan Swift",
+  },
+  {
+    title: "Tom Jones",
+    author: "Henry Fielding",
+  },
+  {
+    title: "Clarissa",
+    author: "Samuel Richardson",
+  },
+  {
+    title: "Tristram Shandy",
+    author: "Laurence Sterne",
+  },
+]);
+
+export const listBooks = store.value;
